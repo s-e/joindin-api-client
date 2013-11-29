7017299efa81ba7745cf69ce7cabad940be86204
Sam Evaskitas <sam.evaskitas@gmail.com>
2013-11-29 17:00:09 +0000

    cleanup syntax errors in description and add more manual tests

--- test.php
+++ test.php
@@ -4,38 +4,53 @@ use Guzzle\Service\Client as GuzzleClient;
 
 require("vendor/autoload.php");
 
+
+
 $api = new GuzzleClient;
 $desc = ServiceDescription::factory('joindin-service-description.json');
+
+$eventId = 34;
+$talkId = 147;
+$userId = 19;
+
 
 $api->setDescription($desc);
 $command = $api->getCommand('getEvents');
 $response = $api->execute($command);
 
-<<<<<<< Updated upstream
-print_r($response);
-=======
 print_r($response);
 unset($command);
 unset($reponse);
 
-$command = $api->getCommand('getEventTalks');
-$response = $api->execute($command, compact($eventId));
-
-print_r($response);
-unset($command);
-unset($reponse);
-
-$command = $api->getCommand('getEventTalkComments');
-$response = $api->execute($command, compact($eventId, $talkId));
+$command = $api->getCommand('getEventTalks', compact('eventId'));
+    try {
+        $response = $api->execute($command);
+    } catch(\Exception $e) {
+        $response = "whoops 1.\n" . $e->getMessage() . "\n ";
+    }
 
 print_r($response);
 unset($command);
 unset($reponse);
 
-$command = $api->getCommand('getUser');
-$response = $api->execute($command, compact($userId));
+$command = $api->getCommand('getEventTalkComments', compact('eventId', 'talkId'));
+    try {
+        $response = $api->execute($command);
+    } catch(\Exception $e) {
+        $response = "whoops 2.\n";
+    }
 
 print_r($response);
 unset($command);
 unset($reponse);
->>>>>>> Stashed changes
+
+$command = $api->getCommand('getUser', compact('userId'));
+    try {
+        $response = $api->execute($command);
+    } catch(\Exception $e) {
+        $response = "whoops 3.\n";
+    }
+
+print_r($response);
+unset($command);
+unset($reponse);
